677074ba108fc107f79a284b60bd77d65b9cc571
Darios <dariosp@wencoindustries.com>
2024-06-16 22:19:28 -0600

    Fix dictionary for generation test for tech and timesheet

--- navigation/navex_report_navigation.py
+++ navigation/navex_report_navigation.py
@@ -2,91 +2,99 @@ from .base_reports_navigation import ReportActions
 import time
 from pywinauto.application import Application
 import pygetwindow as gw
-from config.app_settings import BIGO_STORE_NUMBERS
-from pywinauto.application import Application
+from config.app_settings import *
 from pywinauto.keyboard import SendKeys
-import time
 from pywinauto.keyboard import send_keys
-from datetime import datetime, timedelta
-import os
 
 class BigoReportActions(ReportActions):
     def __init__(self, app, config):
         super().__init__()
-        self.stores = BIGO_STORE_NUMBERS
+        self.stores = BIGO_POS_CTRL_MAP
         self.config = config
         self.app = app
 
+    def perform_action_with_retry(self, action, retries=3, delay=2):
+        for attempt in range(retries):
+            try:
+                action()
+                return
+            except Exception as e:
+                print(f"Error performing action: {e}. Retrying ({attempt + 1}/{retries})...")
+                time.sleep(delay)
+        print("Failed to perform action after multiple retries.")
+
     def select_report(self, report_title):
-        def find_pos_report_name():
+        def action():
+            def find_pos_report_name():
+                window = self.app.window(title="Solera/DST - Big O Home Office 9.5_STD_BGO", control_type="Window")
+                reports_combo = window.child_window(title="cboReports", control_type="ComboBox")
+                combo_box_items = reports_combo.children()
+                for index, item in enumerate(combo_box_items):
+                    item_text = item.window_text().strip()
+                    if report_title == item_text:
+                        return item.window_text()
+
+            def click_item(report_pos_title):
+                window = self.app.window(title="Solera/DST - Big O Home Office 9.5_STD_BGO", control_type="Window")
+                reports_combo = window.child_window(title="cboReports", control_type="ComboBox")
+                item = reports_combo.child_window(title=report_pos_title, control_type="ListItem")
+                if item.exists():
+                    item.click_input()
+                    print(f"{item} clicked")
+
+            def find_combo_distance(pos_report_name):
+                window = self.app.window(title="Solera/DST - Big O Home Office 9.5_STD_BGO", control_type="Window")
+                reports_combo = window.child_window(title="cboReports", control_type="ComboBox")
+                combo_box_items = reports_combo.children()
+                for index, item in enumerate(combo_box_items):
+                    if pos_report_name in item.window_text():
+                        return index
+
             window = self.app.window(title="Solera/DST - Big O Home Office 9.5_STD_BGO", control_type="Window")
+            window.set_focus()
             reports_combo = window.child_window(title="cboReports", control_type="ComboBox")
-            combo_box_items = reports_combo.children()
-            for index, item in enumerate(combo_box_items):
-                item_text = item.window_text().strip()
-                if report_title == item_text:
-                    return item.window_text()
-
-        def click_item(report_pos_title):
-            window = self.app.window(title="Solera/DST - Big O Home Office 9.5_STD_BGO", control_type="Window")
-            reports_combo = window.child_window(title="cboReports", control_type="ComboBox")
-            item = reports_combo.child_window(title=report_pos_title, control_type="ListItem")
-            if item.exists():
-                item.click_input()
-                print(f"{item} clicked")
-
-        def find_combo_distance(pos_report_name):
-            window = self.app.window(title="Solera/DST - Big O Home Office 9.5_STD_BGO", control_type="Window")
-            reports_combo = window.child_window(title="cboReports", control_type="ComboBox")
-            combo_box_items = reports_combo.children()
-            for index, item in enumerate(combo_box_items):
-                if pos_report_name in item.window_text():
-                    return index
-
-
-        window = self.app.window(title="Solera/DST - Big O Home Office 9.5_STD_BGO", control_type="Window")
-        window.set_focus()
-        reports_combo = window.child_window(title="cboReports", control_type="ComboBox")
-        reports_combo.click_input()
-        send_keys("a")
-        time.sleep(.05)
-        send_keys("{ENTER}")
-        time.sleep(1)
-
-        reports_combo.click_input()
-        pos_report_name = find_pos_report_name()
-        print(pos_report_name)
-        first_three_letters = report_title[:3]
-        send_keys(first_three_letters)
-        send_keys("{ENTER}")
-        time.sleep(1)
-
-        print("first search completed")
-
-        reports_combo.click_input()
-        first_distance = find_combo_distance(pos_report_name)
-        if first_distance > 15:
-            print("over 15")
-            down_presses_needed = (first_distance // 5) * 5  # To move in increments of 5
-            for _ in range(down_presses_needed):
-                send_keys("{DOWN}")
-                time.sleep(0.5)
+            reports_combo.click_input()
+            send_keys("a")
+            time.sleep(.05)
             send_keys("{ENTER}")
-        else:
-            clicked = False
-            while not clicked:
+            time.sleep(1)
+
+            reports_combo.click_input()
+            pos_report_name = find_pos_report_name()
+            print(pos_report_name)
+            first_three_letters = report_title[:3]
+            send_keys(first_three_letters)
+            send_keys("{ENTER}")
+            time.sleep(1)
+
+            print("first search completed")
+
+            reports_combo.click_input()
+            first_distance = find_combo_distance(pos_report_name)
+            if first_distance > 15:
+                print("over 15")
+                down_presses_needed = (first_distance // 5) * 5  # To move in increments of 5
+                for _ in range(down_presses_needed):
+                    send_keys("{DOWN}")
+                    time.sleep(0.5)
                 send_keys("{ENTER}")
-                time.sleep(0.5)
-                reports_combo.click_input()
-                click_item(pos_report_name)
-                time.sleep(2)
-                reports_combo.click_input()
-                success = find_combo_distance(pos_report_name)
-                print(success)
-                if success == 0:
+            else:
+                clicked = False
+                while not clicked:
                     send_keys("{ENTER}")
-                    print (f"{report_title} Successfully Selected")
-                    break
+                    time.sleep(0.5)
+                    reports_combo.click_input()
+                    click_item(pos_report_name)
+                    time.sleep(2)
+                    reports_combo.click_input()
+                    success = find_combo_distance(pos_report_name)
+                    print(success)
+                    if success == 0:
+                        send_keys("{ENTER}")
+                        print(f"{report_title} Successfully Selected")
+                        break
+
+        self.perform_action_with_retry(action)
 
     def ts_select_employees(self):
         def action():
@@ -119,7 +127,6 @@ class BigoReportActions(ReportActions):
             window.type_keys(end_date)
             time.sleep(0.5)
         self.perform_action_with_retry(action)
-
 
     def select_generate_report(self):
         def action():
@@ -167,8 +174,8 @@ class BigoReportActions(ReportActions):
             save_button.click_input()
             self.check_existing_file()
             time.sleep(0.5)
-
         self.perform_action_with_retry(action)
+
     def check_existing_file(self):
         def action():
             window = self.app.window(title="Solera/DST - Big O Home Office 9.5_STD_BGO", control_type="Window")
@@ -176,7 +183,6 @@ class BigoReportActions(ReportActions):
             if existing_dialog.exists():
                 yes_button = existing_dialog.child_window(title="Yes", control_type="Button")
                 yes_button.click_input()
-
         self.perform_action_with_retry(action)
 
     def wait_for_report_to_compile(self):
@@ -210,5 +216,3 @@ class BigoReportActions(ReportActions):
                 print(f"Closing Excel window: {window_title}")
                 gw.getWindowsWithTitle(window_title)[0].close()
         self.perform_action_with_retry(action)
-
-
--- tests/generation.py
+++ tests/generation.py
@@ -8,45 +8,46 @@ def test_midas_reports():
 
     # Generate future dates
     test_date = (datetime.today() + timedelta(days=1)).strftime('%Y-%m-%d')
-    future_dates = {store: [test_date] for store in MIDAS_STORE_NUMBERS}
+    future_dates_ss = {store: [test_date] for store in MIDAS_STORE_NUMBERS}
+    future_dates_tech_ts = {store: [(test_date, test_date)] for store in MIDAS_STORE_NUMBERS}
 
     print("Midas Sales Summary Data to be passed:")
-    print(future_dates)
+    print(future_dates_ss)
 
     print("Midas Tech Data to be passed:")
-    print(future_dates)
+    print(future_dates_tech_ts)
 
     print("Midas Timesheet Data to be passed:")
-    print(future_dates)
+    print(future_dates_tech_ts)
 
-    midas_generator = MidasReportGenerator()
-    midas_generator.prepare_pos()
-
-    # Test Sales Summary Report
-    print("Testing Midas Sales Summary Report")
-    midas_generator.generate_ss_reports(future_dates)
-
-    # Test Tech Report
-    print("Testing Midas Tech Report")
-    midas_generator.generate_tech_reports(future_dates)
-
-    # Test Timesheet Report
-    print("Testing Midas Timesheet Report")
-    midas_generator.generate_timesheet_reports(future_dates)
-
-    midas_generator.actions.app.kill()  # Close POS
-    print("Midas Reports Test Completed")
+    # midas_generator = MidasReportGenerator()
+    # midas_generator.prepare_pos()
+    #
+    # # Test Sales Summary Report
+    # print("Testing Midas Sales Summary Report")
+    # midas_generator.generate_ss_reports(future_dates_ss)
+    #
+    # # Test Tech Report
+    # print("Testing Midas Tech Report")
+    # midas_generator.generate_tech_reports(future_dates_tech_ts)
+    #
+    # # Test Timesheet Report
+    # print("Testing Midas Timesheet Report")
+    # midas_generator.generate_timesheet_reports(future_dates_tech_ts)
+    #
+    # midas_generator.actions.app.kill()  # Close POS
+    # print("Midas Reports Test Completed")
 
 def test_bigo_reports():
     print("Testing Bigo Reports")
 
     # Generate future dates
     test_date = (datetime.today() + timedelta(days=1)).strftime('%Y-%m-%d')
-    future_dates = {store: [test_date] for store in BIGO_STORE_NUMBERS}
-    future_dates_list = [test_date]
+    future_dates_ss = {store: [test_date] for store in BIGO_STORE_NUMBERS}
+    future_dates_list = [(test_date, test_date)]
 
     print("Bigo Sales Summary Data to be passed:")
-    print(future_dates)
+    print(future_dates_ss)
 
     print("Bigo Tech Data to be passed:")
     print(future_dates_list)
@@ -54,23 +55,23 @@ def test_bigo_reports():
     print("Bigo Timesheet Data to be passed:")
     print(future_dates_list)
 
-    bigo_generator = BigoReportGenerator()
-    bigo_generator.prepare_pos()
-
-    # Test Sales Summary Report
-    print("Testing Bigo Sales Summary Report")
-    bigo_generator.generate_ss_reports(future_dates)
-
-    # Test Tech Report
-    print("Testing Bigo Tech Report")
-    bigo_generator.generate_tech_reports(future_dates_list)
-
-    # Test Timesheet Report
-    print("Testing Bigo Timesheet Report")
-    bigo_generator.generate_timesheet_reports(future_dates_list)
-
-    bigo_generator.actions.app.kill()  # Close POS
-    print("Bigo Reports Test Completed")
+    # bigo_generator = BigoReportGenerator()
+    # bigo_generator.prepare_pos()
+    #
+    # # Test Sales Summary Report
+    # print("Testing Bigo Sales Summary Report")
+    # bigo_generator.generate_ss_reports(future_dates_ss)
+    #
+    # # Test Tech Report
+    # print("Testing Bigo Tech Report")
+    # bigo_generator.generate_tech_reports(future_dates_list)
+    #
+    # # Test Timesheet Report
+    # print("Testing Bigo Timesheet Report")
+    # bigo_generator.generate_timesheet_reports(future_dates_list)
+    #
+    # bigo_generator.actions.app.kill()  # Close POS
+    # print("Bigo Reports Test Completed")
 
 if __name__ == "__main__":
     test_midas_reports()
